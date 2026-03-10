54bbb428fec7dc6756753aec663f5e655e0bcc2e
Mariusz <casmar90@gmail.com>
2026-03-10 18:58:13 +0100

    add useTitle hook and implement dynamic page titles

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,8 +2,11 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Wrapper from "../components/Wrapper";
+import { useTitle } from "../hooks/useTitle";
 
-export default function App() {
+export default function Login() {
+  useTitle("Login");
+
   return (
     <Wrapper>
       <Header />
