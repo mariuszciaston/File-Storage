8f825da4c4b236db7f5394a6c393b8f90405f846
Mariusz <casmar90@gmail.com>
2026-07-30 10:37:50 +0200

    export Upload component as default

--- client/src/components/Upload.tsx
+++ client/src/components/Upload.tsx
@@ -1,4 +1,4 @@
-function Upload() {
+export default function Upload() {
   async function handleFile(event: React.ChangeEvent<HTMLInputElement>) {
     const file = event.target.files?.[0];
 
@@ -26,5 +26,3 @@ function Upload() {
     />
   );
 }
-
-export default Upload;
