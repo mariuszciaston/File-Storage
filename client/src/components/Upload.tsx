ba160846879488b171181b551e38d671f3025353
Mariusz <casmar90@gmail.com>
2026-07-30 16:16:23 +0200

    Enhance file upload validation and expand accepted file types

--- client/src/components/Upload.tsx
+++ client/src/components/Upload.tsx
@@ -4,6 +4,12 @@ export default function Upload() {
 
     if (!file) return;
 
+    if (file.size > 1024 * 1024) {
+      alert("File size must not exceed 1MB.");
+      event.target.value = "";
+      return;
+    }
+
     const formData = new FormData();
     formData.append("file", file);
 
@@ -19,7 +25,32 @@ export default function Upload() {
 
   return (
     <input
-      accept="image/png, image/jpeg, text/plain"
+      accept="
+  image/*,
+  application/pdf,
+  text/plain,
+
+  application/msword,
+  application/vnd.openxmlformats-officedocument.wordprocessingml.document,
+  application/vnd.ms-excel,
+  application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,
+  application/vnd.ms-powerpoint,
+  application/vnd.openxmlformats-officedocument.presentationml.presentation,
+
+  application/vnd.oasis.opendocument.text,
+  application/vnd.oasis.opendocument.spreadsheet,
+  application/vnd.oasis.opendocument.presentation,
+
+  .doc,
+  .docx,
+  .xls,
+  .xlsx,
+  .ppt,
+  .pptx,
+  .odt,
+  .ods,
+  .odp
+"
       className="block w-full text-sm file:mr-4 file:rounded-lg file:border-0 file:bg-blue-500 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-white hover:file:cursor-pointer"
       onChange={handleFile}
       type="file"
